6ae227e511f0b9670a5924f12931585127104c0c
Rajin Khan <rajin.khan2001@gmail.com>
2025-01-20 16:31:43 +0600

    Start learning React

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing
+function Message() {
+    //JSX code
+    const name = 'Rajin';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
